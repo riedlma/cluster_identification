fcdd4b82054f6d68cfdcae5824803de5296600d8
Martin Riedl <riedlmn@gerfalke2.ims.uni-stuttgart.de>
2019-04-08 14:47:58 +0200

    add script to download embeddings

---
+++ scripts/download_embeddings.sh
@@ -0,0 +1,4 @@
+mkdir embeddings
+wget -O embeddings/year1912.clean.txt.fasttext.100.bin https://zenodo.org/record/2620295/files/year1912.clean.txt.fasttext.100.bin?download=1
+wget -O embeddings/year1912.clean.txt.fasttext.200.bin https://zenodo.org/record/2620295/files/year1912.clean.txt.fasttext.200.bin?download=1
+
